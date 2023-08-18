205019bbd83dce98d341d17305f441e02a5be31d
unknown <isaferroortiz@gmail.com>
2023-08-18 20:05:55 -0300

    Notaf

---
+++ Nota.py
@@ -0,0 +1,12 @@
+nota = int (input ("Digite a nota do aluno: "))
+
+if nota >= 90:
+    print("A - Chuchu Beleza.")
+elif nota >= 80:
+    print ("B - Do balaco baco.")
+elif nota >= 70:
+    print ("C - Que peninha.")
+elif nota >= 60:
+    print ("D - Ai ai ai.")
+else:
+    print ("F - Paia.")
