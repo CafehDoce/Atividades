93e185be4a77b2eb9e8d2cdd4f4fe302e3fc592f
unknown <isaferroortiz@gmail.com>
2023-08-18 19:59:40 -0300

    Idade

---
+++ Idade.py
@@ -0,0 +1,18 @@
+
+#git init
+#git add .
+#git commit -a -m "Ecrever algo"
+#git branch -M main
+#git remote add...
+#git push -u origin main
+
+#git add nome_do arqivo.py
+
+
+idade = int(input("Digite sua idade: "))
+
+if idade >= 18:
+    print ("Você é maior de idade.")
+
+else:
+    print ("Você é menor de idade.")
