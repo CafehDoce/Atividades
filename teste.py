0e21cfbea1fca871f15bf1720aa7a1aaec867d8d
unknown <isaferroortiz@gmail.com>
2023-09-01 20:06:50 -0300

    ativ

--- teste.py
+++ teste.py
@@ -9,3 +9,18 @@ def soma (numero1, numero2):
         print(numero1 + numero2)
 
 soma (2,3)
+
+
+
+def soma2 (numero11, numero2):
+    return numero11 + numero2
+
+resultado = soma2 (3,3)
+
+print (resultado)
+
+
+def exemplo(a, b, c):
+       print(a, b, c)
+
+exemplo(1, 2, 3)
