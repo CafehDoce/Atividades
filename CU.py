ed013099d143c5620f18e13e5898346d125871d6
Isabella F Ortiz <141460524+CafehDoce@users.noreply.github.com>
2023-10-06 21:34:09 -0300

    Add files via upload

---
+++ CU.py
@@ -0,0 +1,17 @@
+class Carro:
+    pneus = 4
+
+    def __init__ (self, marca, modelo):
+        self.marca = marca
+        self.modelo = modelo
+
+carro1 = Carro ('Ford', 'Fiesta')
+carro2 = Carro ('Honda', 'Civic')
+
+print (carro1.pneus)
+print (carro2.pneus)
+
+Carro.pneus = 3
+
+print (carro1.pneus)
+print (carro2.pneus)
